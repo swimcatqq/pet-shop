47ef4d1b120b0664bbdd1d5272555e81a9ff7ec1
syq <1423491534@qq.com>
2023-04-11 16:07:59 +0800

    初始化版本

---
+++ src/content/petDailyNecessities/dailyNecessities.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      这是宠物日常用品界面
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/content/users/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      这是登陆后的主界面
+
+    </div>
+    <RouterView />
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
